7652c0e9bd994c4d20529b26147db3103ab67af7
simone menichetti <simon3menichettui95@gmail.com>
2024-06-26 16:32:46 +0200

    feat:(add dinamic task with v for )

--- javascript/mainjavascript.js
+++ javascript/mainjavascript.js
@@ -9,3 +9,46 @@
 // Visualizzare a fianco ad ogni item ha una “x”: cliccando su di essa, il todo viene rimosso dalla lista.
 // MILESTONE 3
 // Predisporre un campo di input testuale e un pulsante “aggiungi”: cliccando sul pulsante, il testo digitato viene letto e utilizzato per creare un nuovo todo, che quindi viene aggiunto alla lista dei todo esistenti.
+
+
+
+// MILESTONE 1
+// Stampare all’interno di una lista HTML un item per ogni todo.
+
+
+
+const {createApp}= Vue;
+
+
+createApp ({
+    data(){
+        return{
+            newTasks: '',
+            tasks:
+            [
+
+                { 
+                text: 'Compra il succo di murloc',
+                done: false 
+                },
+
+                {
+                text: 'Vai di mainquest',
+                done: false
+                },
+
+                {
+                text: 'Studia le recipe di enginering',
+                done: false
+                }
+            ]
+            
+        }
+        
+    },
+
+    methods:{
+
+    },
+
+}).mount('#app');
